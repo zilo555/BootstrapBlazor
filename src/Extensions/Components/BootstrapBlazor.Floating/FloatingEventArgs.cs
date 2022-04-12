8b905d9c672516ac78da961bf60d6068f342e2cf
developerhan <developerhan@msn.cn>
2022-04-12 11:57:34 +0800

    Define floating layer trigger event Convention

---
+++ src/Extensions/Components/BootstrapBlazor.Floating/FloatingEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 浮动层 触发事件约定
+/// </summary>
+/// <param name="e"></param>
+internal delegate void FloatingTriggerEventHandler(FloatingEventArgs e);
+
+/// <summary>
+/// 浮动层 事件参数
+/// </summary>
+internal sealed class FloatingEventArgs : EventArgs
+{
+    public bool Show { get; }
+
+    public FloatingEventArgs(bool show) => Show = show;
+}
