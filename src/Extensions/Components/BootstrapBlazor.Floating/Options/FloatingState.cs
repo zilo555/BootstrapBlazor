72749495e53a4ec2dce0478d3eca4b7dd3e915e1
developerhan <developerhan@msn.cn>
2022-04-12 11:55:57 +0800

    Define the floating layer position parameters, positioning mode and events that trigger the visibility of the floating layer

---
+++ src/Extensions/Components/BootstrapBlazor.Floating/Options/FloatingState.cs
@@ -0,0 +1,68 @@
+﻿namespace BootstrapBlazor.Components;
+
+/// <summary>
+///  浮动层 位置参数
+/// </summary>
+internal record FloatingState
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public decimal? FloatingTop { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public decimal? FloatingLeft { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public decimal? ArrowTop { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public decimal? ArrowLeft { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? ArrowOffset { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="floatingLeft"></param>
+    /// <param name="floatingTop"></param>
+    public void UpdateFloating(decimal? floatingLeft, decimal? floatingTop)
+    {
+        FloatingLeft = floatingLeft;
+        FloatingTop = floatingTop;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="arrowTop"></param>
+    /// <param name="arrowLeft"></param>
+    /// <param name="arrowOffset"></param>
+    public void UpdateArrow(decimal? arrowLeft, decimal? arrowTop, string? arrowOffset)
+    {
+        ArrowTop = arrowTop;
+        ArrowLeft = arrowLeft;
+        ArrowOffset = arrowOffset;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Reset()
+    {
+        FloatingTop = null;
+        FloatingLeft = null;
+        ArrowTop = null;
+        ArrowLeft = null;
+        ArrowOffset = null;
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.Floating/Options/PositionStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 浮动层 定位模式
+/// </summary>
+public enum PositionStrategy
+{
+    /// <summary>
+    /// 绝对定位
+    /// </summary>
+    [Description("absolute")]
+    Absolute,
+
+    /// <summary>
+    /// 固定定位
+    /// </summary>
+    [Description("fixed")]
+    Fixed,
+}
