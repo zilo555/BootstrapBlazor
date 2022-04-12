7165a33a0337862c69507dd5bc0c2289a6dbe4b0
Argo-Lenovo <argo@163.com>
2022-04-12 16:31:04 +0800

    doc: 增加 FooDemos 示例网页

---
+++ src/BootstrapBlazor.Shared/Samples/FooDemos.razor.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Shared.Common;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BootstrapBlazor.Shared.Samples;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class FooDemos : IAsyncDisposable
+{
+    [Inject]
+    [NotNull]
+    private IJSRuntime? JSRuntime { get; set; }
+
+    [NotNull]
+    private IJSObjectReference? Module { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.TypeScriptTest/ts/FooDemo.js");
+        }
+    }
+
+    private async Task OnClick()
+    {
+        if (Module != null)
+        {
+            await Module.InvokeVoidAsync("bb_foo");
+        }
+    }
+
+    private async Task DisposeAsync(bool disposing)
+    {
+        if (disposing)
+        {
+            if (Module != null)
+            {
+                await Module.DisposeAsync();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsync(true);
+        GC.SuppressFinalize(this);
+    }
+}
