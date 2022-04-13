2894e3b9b4a6b4360f4507ee8136109e748d6ca8
developerhan <developerhan@msn.cn>
2022-04-13 13:52:43 +0800

    Adjust the floating control context parameters to receive the mouse event position to support virtual elements

--- src/Extensions/Components/BootstrapBlazor.Floating/FloatingContext.cs
+++ src/Extensions/Components/BootstrapBlazor.Floating/FloatingContext.cs
@@ -1,5 +1,4 @@
 ﻿namespace BootstrapBlazor.Components;
-
 /// <summary>
 /// 浮动层 引用控制上下文，不能被多个组件同时使用
 /// </summary>
@@ -19,57 +18,67 @@ public sealed class FloatingContext<TRef>
     /// <summary>
     /// 构造函数
     /// </summary>
+    /// <param name="forceTrigger"></param>
+    public FloatingContext(bool forceTrigger) => ForceTrigger = forceTrigger;
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
     /// <param name="reference"></param>
     public FloatingContext(TRef reference) => Reference = reference;
 
     /// <summary>
-    /// 是否初始化组件引用，接收到第一个不为空的对象时设置为 true
+    /// 获得/设置 无关显示状态，强制触发事件
     /// </summary>
-    public bool Initialized { get; private set; }
+    public bool ForceTrigger { get; init; }
 
-    private TRef? _reference;
     /// <summary>
     /// 获得/设置 参照控件或元素对象
     /// </summary>
-    public TRef? Reference
-    {
-        get => _reference;
-        set
-        {
-            _reference = value;
-            if (!Initialized && value != null)
-            {
-                Initialized = true;
-            }
-        }
-    }
+    public TRef? Reference { get; set; }
 
     private bool _show;
     /// <summary>
-    /// 获得/设置 是否显示浮动层
+    /// 获得 是否显示浮动层
     /// </summary>
-    public bool Show
-    {
-        get => _show;
-        set
-        {
-            _show = value;
-            FloatingChanged?.Invoke(new(value));
-        }
-    }
+    public bool Show => _show;
 
     /// <summary>
     /// 切换显示状态
     /// </summary>
-    public void Toggle() => Show = !Show;
+    public void Toggle<TEventArgs>(TEventArgs args) where TEventArgs : EventArgs
+    {
+        if (_show)
+        {
+            Reset(args);
+        }
+        else
+        {
+            Overlay(args);
+        }
+    }
 
     /// <summary>
-    /// 叠加显示
+    /// 叠加显示，虚拟元素需要读取参数中的坐标值
     /// </summary>
-    public void Overlay() => Show = true;
+    public void Overlay<TEventArgs>(TEventArgs args) where TEventArgs : EventArgs
+    {
+        if (_show != true || ForceTrigger)
+        {
+            _show = true;
+            FloatingChanged?.Invoke(FloatingEventArgs.Create(args, true));
+        }
+    }
 
     /// <summary>
     /// 还原默认状态
     /// </summary>
-    public void Reset() => Show = false;
+    public void Reset<TEventArgs>(TEventArgs args) where TEventArgs : EventArgs
+    {
+        if (_show != false || ForceTrigger)
+        {
+            _show = false;
+            FloatingChanged?.Invoke(FloatingEventArgs.Create(args, false));
+        }
+    }
 }
