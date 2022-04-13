50073a2e1a432faa57f74885fea529b6779dd0db
developerhan <developerhan@msn.cn>
2022-04-13 13:52:59 +0800

    Adjust the floating control context parameters to receive the mouse event position to support virtual elements

--- src/Extensions/Components/BootstrapBlazor.Floating/FloatingEventArgs.cs
+++ src/Extensions/Components/BootstrapBlazor.Floating/FloatingEventArgs.cs
@@ -1,4 +1,6 @@
-﻿namespace BootstrapBlazor.Components;
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// 浮动层 触发事件约定
@@ -11,7 +13,54 @@ internal delegate void FloatingTriggerEventHandler(FloatingEventArgs e);
 /// </summary>
 internal sealed class FloatingEventArgs : EventArgs
 {
-    public bool Show { get; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool Show { get; private set; }
+
+    /// <summary>
+    /// 源事件类型
+    /// </summary>
+    public string? Source { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the type of the event.
+    /// </summary>
+    public string? Type { get; private set; }
+
+    /// <summary>
+    /// The X coordinate of the mouse pointer in local (DOM content) coordinates.
+    /// </summary>
+    public double ClientX { get; private set; }
+
+    /// <summary>
+    /// The Y coordinate of the mouse pointer in local (DOM content) coordinates.
+    /// </summary>
+    public double ClientY { get; private set; }
 
     public FloatingEventArgs(bool show) => Show = show;
+
+    public FloatingEventArgs(MouseEventArgs args, bool show) : this(show)
+    {
+        this.Source = "Mouse";
+        this.Type = args.Type;
+        this.ClientX = args.ClientX;
+        this.ClientY = args.ClientY;
+    }
+
+    public FloatingEventArgs(FocusEventArgs args, bool show) : this(show)
+    {
+        this.Source = "Focus";
+        this.Type = args.Type;
+    }
+
+    public static FloatingEventArgs Create<TEventArgs>(TEventArgs args, bool show) where TEventArgs : EventArgs
+    {
+        return args switch
+        {
+            MouseEventArgs mouse => new(mouse, show),
+            FocusEventArgs focus => new(focus, show),
+            _ => new(show)
+        };
+    }
 }
