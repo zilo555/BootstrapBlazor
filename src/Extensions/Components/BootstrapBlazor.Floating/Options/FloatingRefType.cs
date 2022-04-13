66ca68687eb38a0d7cbcbab33b65010aab69e7af
developerhan <developerhan@msn.cn>
2022-04-13 13:50:09 +0800

    Defines the reference element types supported by the floating component

---
+++ src/Extensions/Components/BootstrapBlazor.Floating/Options/FloatingRefType.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 浮动层 参照元素类型
+/// </summary>
+public enum FloatingRefType
+{
+    /// <summary>
+    /// CSS选择器
+    /// </summary>
+    [Description("cssSelecter")]
+    CssSelecter,
+
+    /// <summary>
+    /// HTML元素
+    /// </summary>
+    [Description("elementReference")]
+    ElementReference,
+
+    /// <summary>
+    /// Id组件
+    /// </summary>
+    [Description("idComponentBase")]
+    IdComponentBase,
+}
