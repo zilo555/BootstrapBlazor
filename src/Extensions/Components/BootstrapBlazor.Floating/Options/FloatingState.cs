5eacefb8a39f756978e83c7d3ddf1a3d472a9273
developerhan <developerhan@msn.cn>
2022-04-13 13:50:47 +0800

    Defines the initial state of the floating element of the floating component

--- src/Extensions/Components/BootstrapBlazor.Floating/Options/FloatingState.cs
+++ src/Extensions/Components/BootstrapBlazor.Floating/Options/FloatingState.cs
@@ -1,68 +1,27 @@
-﻿namespace BootstrapBlazor.Components;
+﻿using System.ComponentModel;
+
+namespace BootstrapBlazor.Components;
 
 /// <summary>
-///  浮动层 位置参数
+/// 浮动层 显示状态
 /// </summary>
-internal record FloatingState
+public enum FloatingState
 {
     /// <summary>
-    /// 
+    /// 隐藏状态
     /// </summary>
-    public decimal? FloatingTop { get; set; }
+    [Description("hidden")]
+    Hidden,
 
     /// <summary>
-    /// 
+    /// 不对内容做任何调整，原样显示
     /// </summary>
-    public decimal? FloatingLeft { get; set; }
+    [Description("raw")]
+    Raw,
 
     /// <summary>
-    /// 
+    /// 浮动叠加
     /// </summary>
-    public decimal? ArrowTop { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public decimal? ArrowLeft { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public string? ArrowOffset { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="floatingLeft"></param>
-    /// <param name="floatingTop"></param>
-    public void UpdateFloating(decimal? floatingLeft, decimal? floatingTop)
-    {
-        FloatingLeft = floatingLeft;
-        FloatingTop = floatingTop;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="arrowTop"></param>
-    /// <param name="arrowLeft"></param>
-    /// <param name="arrowOffset"></param>
-    public void UpdateArrow(decimal? arrowLeft, decimal? arrowTop, string? arrowOffset)
-    {
-        ArrowTop = arrowTop;
-        ArrowLeft = arrowLeft;
-        ArrowOffset = arrowOffset;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public void Reset()
-    {
-        FloatingTop = null;
-        FloatingLeft = null;
-        ArrowTop = null;
-        ArrowLeft = null;
-        ArrowOffset = null;
-    }
+    [Description("overlay")]
+    Overlay,
 }
