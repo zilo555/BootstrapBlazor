5a2b3618e15d5e15e1fa59fd73f67a95c7f81b4c
devhxj <1327160718@qq.com>
2022-04-28 13:16:08 +0800

    The extension supports onmouseenter and onmousesave

---
+++ src/BootstrapBlazor/Utils/EventHandlers.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 
+/// </summary>
+[EventHandler( "onmouseenter", typeof( MouseEventArgs ), true, true )]
+[EventHandler( "onmouseleave", typeof( MouseEventArgs ), true, true )]
+public static class EventHandlers
+{
+}
