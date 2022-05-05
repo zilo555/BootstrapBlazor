a2609f4c50b34fcdcbd0cc06fc0d1f6e1527e546
Alex Chow <zhouchuanglin@gmail.com>
2022-05-05 03:01:15 +0200

    页面内容不刷新, 光标乱跳错误
    页面内容不刷新, 光标乱跳错误
    测试步骤,1.点首页之外页面,再切回来首页才会出现数据
    2.选中某段文字,再乱点工具栏 B,H 任何按钮,光标大几率跳到文档末尾或者另一个MD组件

---
+++ b08Menu/BootstrapBlazorApp.Shared/Shared/MainLayout.razor.cs
@@ -0,0 +1,94 @@
+﻿using BootstrapBlazor.Components;
+using BootstrapBlazorApp.Shared.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BootstrapBlazorApp.Shared.Shared;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed partial class MainLayout
+{
+    private bool UseTabSet { get; set; } = true;
+
+    private string Theme { get; set; } = "";
+
+    private bool IsOpen { get; set; }
+
+    private bool IsFixedHeader { get; set; } = true;
+
+    private bool IsFixedFooter { get; set; } = true;
+
+    private bool IsFullSide { get; set; } = true;
+
+    private bool ShowFooter { get; set; } = true;
+
+    private List<MenuItem>? Menus { get; set; } = new List<MenuItem>();
+
+    [Inject]
+    [NotNull]
+    IFreeSql? fsql { get; set; }
+
+    /// <summary>
+    /// OnInitialized 方法
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        //Menus = GetIconSideMenuItems();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            if (fsql.Select<WebPages>().Count() == 0)
+            {
+                var pages = new List<WebPages>(){
+                    new WebPages("首页","/","fa fa-home","001") ,
+                    new WebPages("数据","","fa fa-fw fa-database","002",
+                        new List<WebPages>(new[] {
+                            new WebPages("FetchData","fetchdata","fa fa-fw fa-database","002_001") ,
+                            new WebPages( "Counter","counter","fa fa-fw fa-check-square-o","002_002")  ,
+                            new WebPages("后台管理","admins","fa fa-gears","002_003") ,
+                            new WebPages("页面编辑","editpages","fa fa-gears","002_004") ,
+                        })) ,
+                    new WebPages("Table","table","fa fa-fw fa-table","004") ,
+                    new WebPages("花名册","users","fa fa-fw fa-users","005"),
+                    new WebPages("关于我们", "AboutUs", "fa fa-gears", "006"),
+                    new WebPages("隐私政策", "PrivacyPolicy", "fa fa-gears", "007")
+                };
+
+                var repo = fsql.GetRepository<WebPages>();//仓库类
+                repo.DbContextOptions.EnableAddOrUpdateNavigateList = true; //开启一对多，多对多级联保存功能
+                repo.Insert(pages);
+            }
+            //仅配合文章演示,未做版本控制代码
+            if (fsql.Select<WebPages>().Where(a => a.PageName == "页面编辑").Count() == 0)
+            {
+                fsql.Insert(new WebPages("页面编辑", "editpages", "fa fa-gears", "002_004")).ExecuteAffrows();
+            }
+            if (fsql.Select<WebPages>().Where(a => a.PageName == "关于我们").Count() == 0)
+            {
+                fsql.Insert(new WebPages("关于我们", "AboutUs", "fa fa-gears", "006")).ExecuteAffrows();
+                fsql.Insert(new WebPages("隐私政策", "PrivacyPolicy", "fa fa-gears", "007")).ExecuteAffrows();
+            }
+            Menus = fsql.Select<WebPages>().OrderBy(a => a.Code)
+                    .LeftJoin(d => d.ParentCode == d.Parent!.Code)
+                    .ToList(a => new MenuItem()
+                    {
+                        Text = a.PageName,
+                        Id = a.Code,
+                        Url = a.Url,
+                        ParentId = a.ParentCode,
+                        Icon = a.Icon
+                    }).CascadingMenu().ToList();
+            // 算法获取属性结构数据 .CascadingMenu().ToList()
+            StateHasChanged();
+        }
+    }
+
+}
