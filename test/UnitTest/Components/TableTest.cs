13c444ead69508a1fe983668989a53f0cdbd1751
Argo-Lenovo <argo@163.com>
2022-05-06 23:11:28 +0800

    test: add test

--- test/UnitTest/Components/TableTest.cs
+++ test/UnitTest/Components/TableTest.cs
@@ -42,6 +42,21 @@ public class TableTest : TableTestBase
     }
 
     [Fact]
+    public void Table_Ok3()
+    {
+        var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<Table<Foo>>(pb =>
+            {
+                pb.Add(a => a.Items, Foo.GenerateFoo(localizer));
+            });
+        });
+
+        cut.Contains("table");
+    }
+
+    [Fact]
     public void Table_Ok1()
     {
         var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
